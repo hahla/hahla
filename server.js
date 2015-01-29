e0dd5521f2f607aef5332df53f6b05acfd88679f
michael salmon <michael@hahla.com>
2015-01-28 21:06:47 -0800

    lots of updates

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var express = require('express')
 var app = express()
+// var map = require('map.json')
+
+fs = require('fs')
+fs.readFile('game.json', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  game = JSON.parse(data);
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,6 +20,11 @@ app.post('/hi', function (req, res, next) {
     console.log('asdf');
     res.jsonp({hi: 'world!'});
 });
+
+app.post('/game', function(req, res, next){
+    console.log('in game');
+    res.jsonp(game)
+})
 
 var server = app.listen(3000, 'localhost', function () {
   var host = server.address().address
